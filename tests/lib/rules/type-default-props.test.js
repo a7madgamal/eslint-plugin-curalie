fb3784832eed0bfea93af43e48265713d13498ca
Ahmed Hassanein <ahmed.hassanein@curalie.com>
2022-06-15 12:35:23 +0200

    init

---
+++ tests/lib/rules/type-default-props.test.js
@@ -0,0 +1,43 @@
+const RuleTester = require("eslint").RuleTester;
+const rule = require("../../../lib/rules/type-default-props");
+
+const ruleTester = new RuleTester({
+  parser: require.resolve("@typescript-eslint/parser"),
+  parserOptions: {
+    ecmaVersion: 2018,
+  },
+});
+
+const testsFor = (name, ...args) => ruleTester.run(name, rule, ...args);
+
+const asErrors = [{ messageId: "asAssertion" }];
+const angleBracketErrors = [{ messageId: "angleBracketAssertion" }];
+const nonNullAssertionErrors = [{ messageId: "nonNullAssertion" }];
+
+testsFor("type-default-props", {
+  valid: [
+    { code: "const foo = <const>42" },
+    { code: "const foo = <unknown>42" },
+    { code: "const foo = 42 as const" },
+    { code: "const foo = 42 as unknown" },
+  ],
+  invalid: [
+    { code: "const foo = <number>42", errors: angleBracketErrors },
+    { code: "const foo = 42 as number", errors: asErrors },
+    { code: "const foo = 42 as any", errors: asErrors },
+    {
+      code: `
+          const foo: { a?: number } = { a: 42 };
+          const bar = foo.a! + 1;
+        `,
+      errors: nonNullAssertionErrors,
+    },
+    {
+      code: `
+          const foo: { a?: { b: number } } = { a: { b: 42 } };
+          const bar = foo.a!.b + 1
+      `,
+      errors: nonNullAssertionErrors,
+    },
+  ],
+});
